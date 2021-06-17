e3c148e46a7d7efffe87756e6b00ee81ca0070b4
Blacksterhack <blastertv1001@gmail.com>
2021-06-16 23:29:20 -0500

    quick-sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,11 +1,11 @@
 #include "sort.h"
 
 /**
-* swapping_integers - function that swaps two integers
+* swap_integers - function that swaps two integers
 * @a: First integer
 * @b: Second integer
 */
-void swapping_integers(int *a, int *b)
+void swap_integers(int *a, int *b)
 {
 int c;
 
@@ -16,6 +16,11 @@ c = *a;
 
 /**
 * partition - Lomuto partition scheme for Quick Sort algorithm
+* This function divides the elements of the array
+* that are between First and Last index into
+* smaller(on the left) and bigger(on the right) elements than the pivot.
+* The pivot is always the last element.
+*
 * @array: the array
 * @First: the index of the first element
 * @Last: the index of the last element
@@ -33,7 +38,7 @@ if (array[i] < pivot)
 {
 if (array[i] != array[ipivot])
 {
-swapping_integers(array + i, array + ipivot);
+swap_integers(array + i, array + ipivot);
 print_array((const int *)array, size);
 }
 ipivot++;
@@ -41,7 +46,7 @@ ipivot++;
 }
 if (array[ipivot] != array[Last])
 {
-swapping_integers(array + ipivot, array + Last);
+swap_integers(array + ipivot, array + Last);
 print_array((const int *)array, size);
 }
 return (ipivot);
@@ -70,6 +75,7 @@ QS(array, ipivot + 1, Last, size);
 * using the Quick sort algorithm:
 * the Lomuto partition scheme is implemented and
 * the pivot is always the last element
+*
 * @array: the array of integers
 * @size: the size of the array
 */
